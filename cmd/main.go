ae87123c641b4c761fa21cb068a1e9936c064861
syth0le <chdan565@gmail.com>
2022-04-16 17:45:16 +0300

    implemented main function

--- cmd/main.go
+++ cmd/main.go
@@ -6,22 +6,36 @@ import (
 	"github.com/syth0le/microservice-load-balancer/config"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 var cfg config.Config
 
 func doHealthCheck() {
-	//for _, server := range config.Servers {
-	//if a := server.GetAliveStatus() {
-	//	a = 1
-	//}
-	//pingedURL, err := url.Parse(server.URL)
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//} else {
-	//	server.SetAliveStatus(true)
-	//}
-	//}
+	for _, server := range cfg.Servers {
+		if true == server.GetAliveStatus() {
+			//a = 1
+		}
+		pingedURL, err := url.Parse(server.URL)
+		_ = pingedURL
+		if err != nil {
+			log.Fatal(err.Error())
+		} else {
+			server.SetAliveStatus(true)
+		}
+	}
+}
+
+func loadBalancing(w http.ResponseWriter, r *http.Request) {
+	local := time.Now().Format("15:04:05.000")
+	_, err := fmt.Fprintf(w, "I'm a load balancer %s", local)
+	logString := fmt.Sprintf("[%s] %s - %s", local, r.RemoteAddr, r.RequestURI)
+	log.Println(logString)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
@@ -33,5 +47,15 @@ func main() {
 	if err != nil {
 		return
 	}
-	fmt.Println(cfg)
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", cfg.ProxyPort),
+		Handler: http.HandlerFunc(loadBalancing),
+	}
+
+	go doHealthCheck()
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
